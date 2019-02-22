83399ab77064f282469f071f4b7c5107e9388279
Dustin Neighly <neighlyd@gmail.com>
2019-02-22 12:23:13 -0800

    Added GET and DELETE /checklists/:id and tests for both

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 
+const fs = require('fs');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {bcrypt} = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
+const {Checklist} = require('./models/checklist');
 
 let app = express();
 
@@ -14,6 +16,76 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`)
+app.post('/checklists', (req, res) => {
+    let items = [];
+    if (req.body.items) {
+        items = req.body.items;
+    }
+
+    let checklist = new Checklist({
+        title: req.body.title,
+        items: req.body.items
+    });
+    checklist.save().then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
+
+app.get('/checklists', (req, res) => {
+   Checklist.find().then((checklists) => {
+       res.send({checklists});
+    }, (e) => {
+       res.status(400).send(e);
+   });
+});
+
+app.get('/checklists/:id', (req, res) => {
+   let id = req.params.id;
+
+   // verify that user sent a valid ObjectId.
+   if (!ObjectID.isValid(id)){
+       return res.sendStatus(400);
+   }
+
+   Checklist.findOne({_id: id}).then((checklist) => {
+
+       // no checklist by that id found.
+       if (!checklist){
+           return res.sendStatus(404);
+       }
+
+       res.send({checklist});
+   }).catch((e) => res.sendStatus(400));
+});
+
+app.delete('/checklists/:id', (req, res) => {
+    let id = req.params.id;
+
+    // verify that user sent a valid ObjectId.
+    if (!ObjectID.isValid(id)){
+        return res.sendStatus(400);
+    }
+
+    Checklist.findOneAndDelete({_id: id}).then((checklist) => {
+        if (!checklist){
+            return res.sendStatus(404);
+        }
+        res.send({checklist});
+    }).catch((e) => res.sendStatus(400));
+});
+
+app.listen(port, () => {
+    let now = new Date().toString();
+    let log = `${now}: Server started on port ${port}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        } else {
+            console.log(log);
+        }
+    });
+});
+
+module.exports = {app};
